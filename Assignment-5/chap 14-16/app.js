03555be70a1ecfbb51d91e143266031cfa071055
Danial-Ismail <121781837+Danial-Ismail@users.noreply.github.com>
2023-02-14 07:16:54 -0800

    added js and index files

---
+++ Assignment-5/chap 14-16/app.js
@@ -0,0 +1,161 @@
+//****** Chapter 14-16 ****//
+
+//Question 1
+
+//var studentNames=[]
+//console.log(studentNames)
+
+//Question 2
+
+//var studentNames=new Array()
+//console.log(studentNames)
+
+//Question 3
+
+//var fruits=["Banana","Mango","Peach","Apple"];
+//console.log(fruits)
+
+//Question 4
+
+//var num=[4,5,6,7]
+//console.log(num)
+
+//Question 5
+
+//var boolean=[true,false,true,false]
+//console.log(boolean)
+
+//Question 6
+
+//var mixedArray=[true,"banana",23,false]
+//console.log(mixedArray)
+
+//Question 7
+
+// var qualification=["SSC","HSC","BCS","BS","BCOM","MS","M.PHIL","phD"]
+// console.log(qualification)
+// document.write("<h1>Qualifications</h1>")
+// document.write("1) ",qualification[0])
+// document.write("<br>")
+// document.write("2) ",qualification[1])
+// document.write("<br>")
+// document.write("3) ",qualification[2])
+// document.write("<br>")
+// document.write("4) ",qualification[3])
+// document.write("<br>")
+// document.write("5) ",qualification[4])
+// document.write("<br>")
+// document.write("6) ",qualification[5])
+
+//Question 8
+
+// var studentNames=["Jennifer","Scarlett","Ronaldo"];
+// var scores=[320,230,480]
+// var totalMarks=500
+// var percenrage1=scores[0]/totalMarks*100
+// console.log(percenrage1)
+// var percenrage2=scores[1]/totalMarks*100
+// console.log(percenrage2)
+// var percenrage3=scores[2]/totalMarks*100
+// console.log(percenrage3)
+// document.write("Score of "+studentNames[0]+"  " +"is "+scores[0],".Percenatge: 64% ")
+// document.write("<br>")
+// document.write("Score of "+studentNames[1]+"  " +"is "+scores[1],".Percenatge: 46% ")
+// document.write("<br>")
+// document.write("Score of "+studentNames[2]+"  " +"is "+scores[2],".Percenatge: 96% ")
+
+//Question 9
+
+//a
+
+//var mycolors=["blue","pink","white","orange"]
+//mycolors.unshift(prompt("Enter a color"))
+//console.log(mycolors)
+
+//b
+
+//var mycolors=["blue","pink","white","orange"]
+//mycolors.push(prompt("Enter your color"))
+//console.log(mycolors);
+
+//c
+
+//var mycolors=["blue","pink","white","orange"]
+//mycolors.splice(0,0,"black","purple");
+//console.log(mycolors)
+
+//d
+
+//var mycolors=["blue","pink","white","orange"]
+//mycolors.shift()
+//console.log(mycolors)
+
+//e
+
+//var mycolors=["blue","pink","white","orange"]
+//mycolors.pop()
+//console.log(mycolors)
+
+//f
+
+//var color=prompt("Enter your color")
+//var indx=prompt("Enter your index no")
+//var colors=["blue","pink","white","orange"]
+//colors.splice(indx,0,color)
+//console.log(colors)
+
+//g
+
+//var indx=prompt("Enter your index no")
+//var del=prompt("Enter your deleted value")
+//var colors=["blue","pink","white","orange"]
+//colors.splice(indx,del)
+//console.log(colors)
+
+//Question 10
+
+//var studentScores=[320,230,480,120]
+//studentScores.sort()
+//console.log(studentScores)
+
+//Question 11
+
+//cities = ["khi","multan","hyd","faislabad","peshawar"];
+//newArray= cities.slice(1,4);
+//console.log(newArray)
+
+//Question 12
+
+//var arr=["This","is","my","cat"];
+//joinArray=arr.join()
+//console.log(joinArray)
+
+//Question 13
+
+// var array=["Milk","Butter","Bread","Eggs"]
+// delValue=array.shift()
+// console.log(delValue)
+// delValue=array.shift()
+// console.log(delValue)
+// delValue=array.shift()
+// console.log(delValue)
+// delValue=array.shift()
+// console.log(delValue)
+
+//Question 14
+
+// var array=["Milk","Butter","Bread","Eggs"]
+// delValue=array.pop()
+// console.log(delValue)
+// delValue=array.pop()
+// console.log(delValue)
+// delValue=array.pop()
+// console.log(delValue)
+// delValue=array.pop()
+// console.log(delValue)
+
+//Question 15
+
+var phoneMftrs=["Apple","Samsung","Motorola","Nokia","Sony","Haier"]
+document.write("<select><option value>(Apple)</option value></select>")
+document.write("<select><option value>(Samsung)</option value></select>")
